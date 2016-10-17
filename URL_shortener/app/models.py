5a49ae315da81053979f9ed4c8fb200e460c8d10
ConnorFieldUser <conquest.field@gmail.com>
2016-10-17 17:34:54 -0400

    migrated

--- URL_shortener/app/models.py
+++ URL_shortener/app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+from datetime import datetime, timedelta
+
+from django.contrib.auth.models import User
+
+from django.db.models.signals import post_save
+
+from django.dispatch import receiver
+
 # Create your models here.
+
+# class Click(models.Model):
+    # user = models.ManyToManyField(auth.user)
+
+class Bookmark(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=50)
+
+
+
+class Profile(models.Model):
+
+    user = models.OneToOneField('auth.User')
+    bookmark = models.ManyToManyField('Bookmark')
+
+    @receiver(post_save, sender='auth.User')
+    def create_user_profile(**kwargs):
+        created = kwargs.get('created')
+        instance = kwargs.get('instance')
+        if created:
+            Profile.objects.create(user=instance)
