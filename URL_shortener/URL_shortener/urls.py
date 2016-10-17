ce31c37052fe80b1352d044b5a1d1948f9316efe
ConnorFieldUser <conquest.field@gmail.com>
2016-10-17 16:18:16 -0400

    added some base stuff

--- URL_shortener/URL_shortener/urls.py
+++ URL_shortener/URL_shortener/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+from app.views import index_view, UserCreateView, UserListView, UserDetailView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('^', include('django.contrib.auth.urls')),
+    url(r'^$', index_view, name="index_view"),
+    url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view"),
+    url(r'^users/$', UserListView.as_view(), name="user_list_view"),
+    url(r'^users/(?P<pk>\d+)/$', UserDetailView.as_view(), name="user_detail_view"),
+    # (?P<short_url>\w+)
 ]
--- URL_shortener/app/views.py
+++ URL_shortener/app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
+from django.contrib.auth.models import User
+
+from django.contrib.auth.forms import UserCreationForm
+
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
+
 # Create your views here.
+
+def index_view(request):
+    context = {
+    }
+    return render(request, 'index.html', context)
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = "/"
+
+
+class UserListView(ListView):
+    template_name = "users.html"
+    model = User
+
+
+class UserDetailView(ListView):
+    model = User
