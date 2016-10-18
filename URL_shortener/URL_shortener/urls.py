ba0936d5f9e3c0102333cd840c04dfb5e6edda9b
ConnorFieldUser <conquest.field@gmail.com>
2016-10-18 10:55:49 -0400

    unfinished

--- URL_shortener/URL_shortener/urls.py
+++ URL_shortener/URL_shortener/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view"),
     url(r'^users/$', UserListView.as_view(), name="user_list_view"),
     url(r'^users/(?P<pk>\d+)/$', UserDetailView.as_view(), name="user_detail_view"),
+    url(r'^P<new_url>/w+)/$')
     # (?P<short_url>\w+)
 ]
--- URL_shortener/app/views.py
+++ URL_shortener/app/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User
 
-from app.models import Bookmark, Profile
+from app.models import Bookmark  # , Profile
 
 from django.contrib.auth.forms import UserCreationForm
 
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView
 
+from django.http import HttpResponseRedirect
+
 # Create your views here.
+
 
 def index_view(request):
     context = {
@@ -30,3 +33,10 @@ class UserListView(ListView):
 
 class UserDetailView(ListView):
     model = User
+
+
+def ShortView(View):
+    def get(self, request, new_url):
+        new = self.kwargs['new_url']
+        full = Bookmark.objects.get(neew_url=new)
+        return HttpResponseRedirect(full.url)
