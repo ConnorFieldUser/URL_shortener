f63e17636e3bcaf00806b5ce54a9c86065ebdb79
ConnorFieldUser <conquest.field@gmail.com>
2016-10-17 20:13:06 -0400

    commit

--- URL_shortener/app/views.py
+++ URL_shortener/app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User
 
+from app.models import Bookmark, Profile
+
 from django.contrib.auth.forms import UserCreationForm
 
 from django.views.generic.edit import CreateView
